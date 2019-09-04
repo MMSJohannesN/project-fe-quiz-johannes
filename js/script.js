eed2567aa579cb46a2c5e675af89c28bf79a45d1
Neumeier <neumeierj@media-saturn.com>
2019-09-04 17:22:02 +0200

    new version of my quiz with nearly finished functionalities. Layout and right question database are the missing points comming soon. - Johannes

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ nextbutton.onclick = function() {
 
     fragenrundenummer += 1;
 
-    if (fragenrundenummer === 2) {
+    if (fragenrundenummer === 2 && chosenAnswer !== "AAAAA") {
         //Zweite Quizfrage
 
         document.getElementById("question").innerHTML = fragerunde[1].question;
@@ -24,7 +24,8 @@ nextbutton.onclick = function() {
         document.getElementById("answer2").innerHTML = fragerunde[1].answer2;
         document.getElementById("answer3").innerHTML = fragerunde[1].answer3;
         document.getElementById("answer4").innerHTML = fragerunde[1].answer4;
-    } else if (fragenrundenummer === 3) {
+        chosenAnswer = "AAAAA";
+    } else if (fragenrundenummer === 3 && chosenAnswer !== "AAAAA") {
         //Dritte Quizfrage
         document.getElementById("question").innerHTML = fragerunde[2].question;
         
@@ -32,7 +33,8 @@ nextbutton.onclick = function() {
         document.getElementById("answer2").innerHTML = fragerunde[2].answer2;
         document.getElementById("answer3").innerHTML = fragerunde[2].answer3;
         document.getElementById("answer4").innerHTML = fragerunde[2].answer4;
-    } else if (fragenrundenummer === 4) {
+        chosenAnswer = "AAAAA";
+    } else if (fragenrundenummer === 4 && chosenAnswer !== "AAAAA") {
         //Vierte Quizfrage
         document.getElementById("question").innerHTML = fragerunde[3].question;
         
@@ -40,7 +42,8 @@ nextbutton.onclick = function() {
         document.getElementById("answer2").innerHTML = fragerunde[3].answer2;
         document.getElementById("answer3").innerHTML = fragerunde[3].answer3;
         document.getElementById("answer4").innerHTML = fragerunde[3].answer4;
-    } else if (fragenrundenummer === 5) {
+        chosenAnswer = "AAAAA";
+    } else if (fragenrundenummer === 5 && chosenAnswer !== "AAAAA") {
         //Fünfte Quizfrage
         document.getElementById("question").innerHTML = fragerunde[4].question;
         
@@ -48,7 +51,8 @@ nextbutton.onclick = function() {
         document.getElementById("answer2").innerHTML = fragerunde[4].answer2;
         document.getElementById("answer3").innerHTML = fragerunde[4].answer3;
         document.getElementById("answer4").innerHTML = fragerunde[4].answer4;
-    } else if (fragenrundenummer === 6) {
+        chosenAnswer = "AAAAA";
+    } else if (fragenrundenummer === 6 && chosenAnswer !== "AAAAA") {
         //Sechste Quizfrage
         document.getElementById("question").innerHTML = fragerunde[5].question;
         
@@ -56,7 +60,8 @@ nextbutton.onclick = function() {
         document.getElementById("answer2").innerHTML = fragerunde[5].answer2;
         document.getElementById("answer3").innerHTML = fragerunde[5].answer3;
         document.getElementById("answer4").innerHTML = fragerunde[5].answer4;
-    } else if (fragenrundenummer === 7) {
+        chosenAnswer = "AAAAA";
+    } else if (fragenrundenummer === 7 && chosenAnswer !== "AAAAA") {
         //Siebte Quizfrage
         document.getElementById("question").innerHTML = fragerunde[6].question;
         
@@ -64,7 +69,8 @@ nextbutton.onclick = function() {
         document.getElementById("answer2").innerHTML = fragerunde[6].answer2;
         document.getElementById("answer3").innerHTML = fragerunde[6].answer3;
         document.getElementById("answer4").innerHTML = fragerunde[6].answer4;
-    } else if (fragenrundenummer === 8) {
+        chosenAnswer = "AAAAA";
+    } else if (fragenrundenummer === 8 && chosenAnswer !== "AAAAA") {
         //Achte Quizfrage
         document.getElementById("question").innerHTML = fragerunde[7].question;
         
@@ -72,7 +78,8 @@ nextbutton.onclick = function() {
         document.getElementById("answer2").innerHTML = fragerunde[7].answer2;
         document.getElementById("answer3").innerHTML = fragerunde[7].answer3;
         document.getElementById("answer4").innerHTML = fragerunde[7].answer4;
-    } else if (fragenrundenummer === 9) {
+        chosenAnswer = "AAAAA";
+    } else if (fragenrundenummer === 9 && chosenAnswer !== "AAAAA") {
         //Neunte Quizfrage
         document.getElementById("question").innerHTML = fragerunde[8].question;
         
@@ -80,7 +87,8 @@ nextbutton.onclick = function() {
         document.getElementById("answer2").innerHTML = fragerunde[8].answer2;
         document.getElementById("answer3").innerHTML = fragerunde[8].answer3;
         document.getElementById("answer4").innerHTML = fragerunde[8].answer4;
-    } else if (fragenrundenummer === 10) {
+        chosenAnswer = "AAAAA";
+    } else if (fragenrundenummer === 10 && chosenAnswer !== "AAAAA") {
         //Zehnte Quizfrage
         document.getElementById("question").innerHTML = fragerunde[9].question;
         
@@ -88,21 +96,8 @@ nextbutton.onclick = function() {
         document.getElementById("answer2").innerHTML = fragerunde[9].answer2;
         document.getElementById("answer3").innerHTML = fragerunde[9].answer3;
         document.getElementById("answer4").innerHTML = fragerunde[9].answer4;
-    } else {
-        let falsch = 0;
-        for (let y = 0; y < fragerunde.length; y++) {
-            if (fragerunde[y].chosen === fragerunde[y].result) {
-                console.log(`Your result is: TRUE! Your choice "${fragerunde[y].chosen}" was right ;)`);
-            } else {
-                console.log(`Your result is: false. Your choice "${fragerunde[y].chosen}" was wrong. Right answer is "${fragerunde[y].result}".`);
-                falsch += 1;
-            }
-        }
-        let output = 10 - falsch;
-        console.log(`You had ${output} answers right!`);
-        if (output >= 8) {
-            console.log("Great Job! You get a 5% discount on MediaMarkt.de for the next 24 hours :)");
-        }
+        chosenAnswer = "AAAAA";
+    } else if (fragenrundenummer > 10 && chosenAnswer !== "AAAAA") {
 
         answer1.parentNode.removeChild(answer1);
         answer2.parentNode.removeChild(answer2);
@@ -111,17 +106,165 @@ nextbutton.onclick = function() {
         nextbutton.parentNode.removeChild(nextbutton);
 
         document.getElementById("question").innerHTML = "Your results:";
+        // TH
+        let th = document.getElementById("myTh1");
+        let thText = document.createTextNode("Your result is:");
+        th.appendChild(thText);
+        th = document.getElementById("myTh2");
+        thText = document.createTextNode("Your choice was:");
+        th.appendChild(thText);
+        th = document.getElementById("myTh3");
+        thText = document.createTextNode("The solution is:");
+        th.appendChild(thText);
+        // TB
+        let falsch = 0;
+        let booleanResult = fragerunde[0].chosen === fragerunde[0].result;
+        if (booleanResult === false) {falsch += 1}
+        let td = document.getElementById("myTd1");
+        let tdText = document.createTextNode(booleanResult);
+        td.appendChild(tdText);
+        td = document.getElementById("myTd2");
+        tdText = document.createTextNode(fragerunde[0].chosen);
+        td.appendChild(tdText);
+        td = document.getElementById("myTd3");
+        tdText = document.createTextNode(fragerunde[0].result);
+        td.appendChild(tdText);
+        booleanResult = fragerunde[1].chosen === fragerunde[1].result;
+        if (booleanResult === false) {falsch += 1}
+        td = document.getElementById("myTd4");
+        tdText = document.createTextNode(booleanResult);
+        td.appendChild(tdText);
+        td = document.getElementById("myTd5");
+        tdText = document.createTextNode(fragerunde[1].chosen);
+        td.appendChild(tdText);
+        td = document.getElementById("myTd6");
+        tdText = document.createTextNode(fragerunde[1].result);
+        td.appendChild(tdText);
+        booleanResult = fragerunde[2].chosen === fragerunde[2].result;
+        if (booleanResult === false) {falsch += 1}
+        td = document.getElementById("myTd7");
+        tdText = document.createTextNode(booleanResult);
+        td.appendChild(tdText);
+        td = document.getElementById("myTd8");
+        tdText = document.createTextNode(fragerunde[2].chosen);
+        td.appendChild(tdText);
+        td = document.getElementById("myTd9");
+        tdText = document.createTextNode(fragerunde[2].result);
+        td.appendChild(tdText);
+        booleanResult = fragerunde[3].chosen === fragerunde[3].result;
+        if (booleanResult === false) {falsch += 1}
+        td = document.getElementById("myTd10");
+        tdText = document.createTextNode(booleanResult);
+        td.appendChild(tdText);
+        td = document.getElementById("myTd11");
+        tdText = document.createTextNode(fragerunde[3].chosen);
+        td.appendChild(tdText);
+        td = document.getElementById("myTd12");
+        tdText = document.createTextNode(fragerunde[3].result);
+        td.appendChild(tdText);
+        booleanResult = fragerunde[4].chosen === fragerunde[4].result;
+        if (booleanResult === false) {falsch += 1}
+        td = document.getElementById("myTd13");
+        tdText = document.createTextNode(booleanResult);
+        td.appendChild(tdText);
+        td = document.getElementById("myTd14");
+        tdText = document.createTextNode(fragerunde[4].chosen);
+        td.appendChild(tdText);
+        td = document.getElementById("myTd15");
+        tdText = document.createTextNode(fragerunde[4].result);
+        td.appendChild(tdText);
+        booleanResult = fragerunde[5].chosen === fragerunde[5].result;
+        if (booleanResult === false) {falsch += 1}
+        td = document.getElementById("myTd16");
+        tdText = document.createTextNode(booleanResult);
+        td.appendChild(tdText);
+        td = document.getElementById("myTd17");
+        tdText = document.createTextNode(fragerunde[5].chosen);
+        td.appendChild(tdText);
+        td = document.getElementById("myTd18");
+        tdText = document.createTextNode(fragerunde[5].result);
+        td.appendChild(tdText);
+        booleanResult = fragerunde[6].chosen === fragerunde[6].result;
+        if (booleanResult === false) {falsch += 1}
+        td = document.getElementById("myTd19");
+        tdText = document.createTextNode(booleanResult);
+        td.appendChild(tdText);
+        td = document.getElementById("myTd20");
+        tdText = document.createTextNode(fragerunde[6].chosen);
+        td.appendChild(tdText);
+        td = document.getElementById("myTd21");
+        tdText = document.createTextNode(fragerunde[6].result);
+        td.appendChild(tdText);
+        booleanResult = fragerunde[7].chosen === fragerunde[7].result;
+        if (booleanResult === false) {falsch += 1}
+        td = document.getElementById("myTd22");
+        tdText = document.createTextNode(booleanResult);
+        td.appendChild(tdText);
+        td = document.getElementById("myTd23");
+        tdText = document.createTextNode(fragerunde[7].chosen);
+        td.appendChild(tdText);
+        td = document.getElementById("myTd24");
+        tdText = document.createTextNode(fragerunde[7].result);
+        td.appendChild(tdText);
+        booleanResult = fragerunde[8].chosen === fragerunde[8].result;
+        if (booleanResult === false) {falsch += 1}
+        td = document.getElementById("myTd25");
+        tdText = document.createTextNode(booleanResult);
+        td.appendChild(tdText);
+        td = document.getElementById("myTd26");
+        tdText = document.createTextNode(fragerunde[8].chosen);
+        td.appendChild(tdText);
+        td = document.getElementById("myTd27");
+        tdText = document.createTextNode(fragerunde[8].result);
+        td.appendChild(tdText);
+        booleanResult = fragerunde[9].chosen === fragerunde[9].result;
+        if (booleanResult === false) {falsch += 1}
+        td = document.getElementById("myTd28");
+        tdText = document.createTextNode(booleanResult);
+        td.appendChild(tdText);
+        td = document.getElementById("myTd29");
+        tdText = document.createTextNode(fragerunde[9].chosen);
+        td.appendChild(tdText);
+        td = document.getElementById("myTd30");
+        tdText = document.createTextNode(fragerunde[9].result);
+        td.appendChild(tdText);
 
+        let output = 10 - falsch;
+        let paragraph = document.getElementById("paragraph");
+        let paragraphText = document.createTextNode(`You had ${output} answers right!`);
+        paragraph.appendChild(paragraphText);
+
+        if (output <= 3) {
+            let div1 = document.getElementById("div1");
+            let div1Text = document.createTextNode("You were very bad :D");
+            div1.appendChild(div1Text);
+        } else if (output <= 7) {
+            div1 = document.getElementById("div1");
+            div1Text = document.createTextNode("You were not good bad also not bad :P");
+            div1.appendChild(div1Text);
+        } else if (output <= 9){
+            div1 = document.getElementById("div1");
+            div1Text = document.createTextNode("You are very good!");
+            div1.appendChild(div1Text);
+        } else {
+            div1 = document.getElementById("div1");
+            div1Text = document.createTextNode("You are the BEST!!!");
+            div1.appendChild(div1Text);
+        }
+        chosenAnswer = "AAAAA";
+   } else {
+    alert("You have to chose a value!");
+    fragenrundenummer -= 1;
    }
 }
 
-
-// Get Value and match it if it´s right or wrong and do output
+let chosenAnswer = "AAAAA";
+// Get Value and match it if it´s right or wrong and save it in the background
 answer1Chosen = document.getElementById('answer1');
 answer1.onclick = function getValue() {
     let i = fragenrundenummer -1;
     document.getElementById("answer1").value = fragerunde[i].answer1;
-    let chosenAnswer = fragerunde[i].answer1;
+    chosenAnswer = fragerunde[i].answer1;
     fragerunde[i].chosen = chosenAnswer;
 }
 
@@ -129,7 +272,7 @@ answer2Chosen = document.getElementById('answer2');
 answer2.onclick = function getValue() {
     let i = fragenrundenummer -1;
     document.getElementById("answer2").value = fragerunde[i].answer2;
-    let chosenAnswer = fragerunde[i].answer2;
+    chosenAnswer = fragerunde[i].answer2;
     fragerunde[i].chosen = chosenAnswer;
 }
 
@@ -137,7 +280,7 @@ answer3Chosen = document.getElementById('answer3');
 answer3.onclick = function getValue() {
     let i = fragenrundenummer -1;
     document.getElementById("answer3").value = fragerunde[i].answer3;
-    let chosenAnswer = fragerunde[i].answer3;
+    chosenAnswer = fragerunde[i].answer3;
     fragerunde[i].chosen = chosenAnswer;
 }
 
@@ -145,51 +288,26 @@ answer4Chosen = document.getElementById('answer4');
 answer4.onclick = function getValue() {
     let i = fragenrundenummer -1;
     document.getElementById("answer4").value = fragerunde[i].answer4;
-    let chosenAnswer = fragerunde[i].answer4;
+    chosenAnswer = fragerunde[i].answer4;
     fragerunde[i].chosen = chosenAnswer;
 }
 
 
 
-/* 
-//--------> Wasn´t working anymore after my get value code..... :(
-// Select Item with color 
-let answer1button = document.getElementById("answer1");
-    answer1button.onclick = function() {
-    document.getElementById("answer1").style.color = "blue";
 
-    document.getElementById("answer2").style.color = null;
-    document.getElementById("answer3").style.color = null;
-    document.getElementById("answer4").style.color = null;
-}
 
-let answer2button = document.getElementById("answer2");
-    answer2button.onclick = function() {
-    document.getElementById("answer2").style.color = "blue";
-    
-    document.getElementById("answer1").style.color = null;
-    document.getElementById("answer3").style.color = null;
-    document.getElementById("answer4").style.color = null;
-}
 
-let answer3button = document.getElementById("answer3");
-    answer3button.onclick = function() {
-    document.getElementById("answer3").style.color = "blue";
 
-    document.getElementById("answer1").style.color = null;
-    document.getElementById("answer2").style.color = null;
-    document.getElementById("answer4").style.color = null;
-}
 
-let answer4button = document.getElementById("answer4");
-    answer4button.onclick = function() {
-    document.getElementById("answer4").style.color = "blue";
 
-    document.getElementById("answer1").style.color = null;
-    document.getElementById("answer2").style.color = null;
-    document.getElementById("answer3").style.color = null;
-}
-*/
+
+
+
+
+
+
+
+
 
 
 
